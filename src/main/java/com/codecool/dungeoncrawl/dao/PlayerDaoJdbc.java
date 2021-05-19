a33222a0641d49cfa20bfd9c4b5e5a61f619b4d0
Mate Magyar <mate.m.magyar@gmail.com>
2021-05-19 21:42:43 +0200

    Update PlayerDaoJdbc class add() with inventory sample code

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -16,13 +16,20 @@ public class PlayerDaoJdbc implements PlayerDao {
     @Override
     public void add(PlayerModel player) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "INSERT INTO player (id, player_name, hp, x, y) VALUES (?, ?, ?, ?, ?)";
+            String sql =
+                "INSERT INTO player (id, player_name, hp, x, y, purse, item_hand_r, item_hand_l, item_head, item_chest) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, player.getUuid());
             statement.setString(2, player.getPlayerName());
             statement.setInt(3, player.getHp());
             statement.setInt(4, player.getX());
             statement.setInt(5, player.getY());
+            statement.setInt(6, 0);
+            statement.setInt(7, 0);
+            statement.setInt(8, 0);
+            statement.setInt(9, 0);
+            statement.setInt(10, 0);
             statement.executeUpdate();
 //            ResultSet resultSet = statement.getGeneratedKeys();
 //            resultSet.next();
